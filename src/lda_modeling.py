5d8039f3040aab5328b0dbafb2734e309350b071
Anna <anna.dai@duke.edu>
2021-12-06 18:46:03 -0500

    some update

--- src/lda_modeling.py
+++ src/lda_modeling.py
@@ -1,7 +1,7 @@
 #%%
 import pandas as pd
 import joblib
-from torchnlp.encoder import WhitespaceEncoder
+from torchnlp.encoders.text import WhitespaceEncoder
 
 with open("../artefacts/encoder.pickle", "rb") as f:
     encoder: WhitespaceEncoder = joblib.load(f)
