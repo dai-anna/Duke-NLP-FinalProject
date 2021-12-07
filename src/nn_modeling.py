ae8122035519d103d1ea93e9b81c76e312b5156a
Anna <anna.dai@duke.edu>
2021-12-06 20:22:36 -0500

    first dab

--- src/nn_modeling.py
+++ src/nn_modeling.py
@@ -1,1 +1,21 @@
+#%%
 import torch
+import torch.nn as nn
+from torchnlp.encoders.text import WhitespaceEncoder
+import joblib
+
+#%%
+encoder = joblib.load("../artefacts/encoder.pickle")
+X = 
+
+embedding_dim = 50
+hidden_size = 500
+
+#%%
+# pad my inputs with zeros
+nn.utils.rnn.pad_sequence(sequences = X_validation, batch_first=False, padding_value=0.0)
+#%%
+
+model = nn.Sequential(
+    nn.Embedding(num_embeddings=encoder.vocab_size, embedding_dim=embedding_dim),
+)
