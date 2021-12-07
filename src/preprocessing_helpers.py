736ab8bf45d740a788377c6c7600ffd841331dae
Moritz Wilksch <moritz.wilksch@duke.edu>
2021-12-06 20:24:17 -0500

    add mode

--- src/preprocessing_helpers.py
+++ src/preprocessing_helpers.py
@@ -2,11 +2,13 @@
 import pandas as pd
 from torchnlp.encoders.text import WhitespaceEncoder
 
-def encode_dataframe(encoder: WhitespaceEncoder, data: pd.DataFrame) -> tuple[list, pd.Series]:
+def encode_dataframe(encoder: WhitespaceEncoder, data: pd.DataFrame, mode="pytorch") -> tuple[list, pd.Series]:
     encoded_tweets = [encoder.encode(tweet) for tweet in data['tweet']]
-    encoded_tweets = [
-        " ".join(str(x) for x in encoded_tweets[idx].numpy())
-        for idx in range(len(encoded_tweets))
-    ]
+
+    if mode == "sklearn":
+        encoded_tweets = [
+            " ".join(str(x) for x in encoded_tweets[idx].numpy())
+            for idx in range(len(encoded_tweets))
+        ]
 
     return encoded_tweets, data['hashtag']
