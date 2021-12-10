7865a2d4c3d068982ecb636c6c4225f15cbe5609
Moritz Wilksch <moritz.wilksch@duke.edu>
2021-12-10 12:21:33 -0500

    refactor retrain

--- src/tf_train_model_with_best_params.py
+++ src/tf_train_model_with_best_params.py
@@ -64,91 +64,153 @@ FINAL_PARAMS = {
 model = get_compiled_model(**FINAL_PARAMS, learning_rate=LEARNING_RATE)
 
 #%%
+TRAIN_ON_SYNTHETIC = False  # Set to True to train on synthetic data
+CONTINUE_ON_REAL = False  # Set to True to continue training on real data. Needs TRAIN_ON_SYNTHETIC to be True.
+JUST_ON_REAL = True  # Set to True to ONLY train on real data.
+
+assert TRAIN_ON_SYNTHETIC or CONTINUE_ON_REAL or JUST_ON_REAL
+assert not (TRAIN_ON_SYNTHETIC and JUST_ON_REAL)
+assert not (CONTINUE_ON_REAL and JUST_ON_REAL)
+assert TRAIN_ON_SYNTHETIC if CONTINUE_ON_REAL else True
+
+#%%
 # ----------------------------------------- Synthetic Data -----------------------------------------
-synth_train_dataset = tf.data.Dataset.from_tensor_slices(
-    (xtrain, ytrain.cat.codes.values)
-).batch(BATCH_SIZE)
-synth_val_dataset = tf.data.Dataset.from_tensor_slices(
-    (xval, yval.cat.codes.values)
-).batch(BATCH_SIZE)
+if TRAIN_ON_SYNTHETIC:
+    synth_train_dataset = tf.data.Dataset.from_tensor_slices(
+        (xtrain, ytrain.cat.codes.values)
+    ).batch(BATCH_SIZE)
+    synth_val_dataset = tf.data.Dataset.from_tensor_slices(
+        (xval, yval.cat.codes.values)
+    ).batch(BATCH_SIZE)
+
+    #%%
+    # Decide on learning rate based on plot HERE:
+    LEARNING_RATE = 10 ** -2.5
+    model = get_compiled_model(**FINAL_PARAMS, learning_rate=LEARNING_RATE)
+
+    #%%
+    # train model on synth data
+    early_stopping_cb = tf.keras.callbacks.EarlyStopping(
+        monitor="val_accuracy", patience=3, restore_best_weights=True
+    )
+
+    # --------------------- Fit the model ---------------------#
+    REFIT_MODEL = False
+
+    if REFIT_MODEL:
+        hist = model.fit(
+            synth_train_dataset,
+            validation_data=synth_val_dataset,
+            epochs=NUM_EPOCHS,
+            callbacks=[early_stopping_cb],
+        )
+
+        val_loss, val_accuracy = model.evaluate(synth_val_dataset)
+        print(f"[INFO] Best validation acc = {val_accuracy}")
+        # pd.DataFrame(hist.history)[["loss", "val_loss"]].plot(figsize=(8, 5))
+
+        model.save_weights("../artefacts/model_synthdata.hdf5")
+
+        if SAVE_TO_S3:
+            bucket.upload_file(
+                "../artefacts/model_synthdata.hdf5", "artefacts/model_synthdata.hdf5"
+            )
+        print("[INFO] Model saved to disk & uploaded to S3.")
+    else:
+        bucket.download_file(
+            "artefacts/model_synthdata.hdf5", "../artefacts/model_synthdata.hdf5"
+        )
+        model.load_weights("../artefacts/model_synthdata.hdf5")
 
 #%%
-# Decide on learning rate based on plot HERE:
-LEARNING_RATE = 10 ** -2.5
-model = get_compiled_model(**FINAL_PARAMS, learning_rate=LEARNING_RATE)
+# ----------------------------- Continue on Real Data -----------------------------
+if CONTINUE_ON_REAL:
+    train = pd.read_parquet("../data/train.parquet")
+    val = pd.read_parquet("../data/val.parquet")
+    test = pd.read_parquet("../data/test.parquet")
 
-#%%
-# train model on synth data
-early_stopping_cb = tf.keras.callbacks.EarlyStopping(
-    monitor="val_accuracy", patience=3, restore_best_weights=True
-)
+    xtrain, ytrain = encode_dataframe(encoder, data=train, mode="pytorch")
+    xval, yval = encode_dataframe(encoder, data=val, mode="pytorch")
+    xtest, ytest = encode_dataframe(encoder, data=test, mode="pytorch")
 
-# --------------------- Fit the model ---------------------#
-REFIT_MODEL = False
+    # Pad my input sequence with zeros
+    xtrain = nn.utils.rnn.pad_sequence(
+        sequences=xtrain, batch_first=True, padding_value=0.0
+    )
+    xval = nn.utils.rnn.pad_sequence(sequences=xval, batch_first=True, padding_value=0.0)
+    xtest = nn.utils.rnn.pad_sequence(
+        sequences=xtest, batch_first=True, padding_value=0.0
+    )
 
-if REFIT_MODEL:
+    train_dataset = tf.data.Dataset.from_tensor_slices(
+        (xtrain, ytrain.cat.codes.values)
+    ).batch(BATCH_SIZE)
+    val_dataset = tf.data.Dataset.from_tensor_slices((xval, yval.cat.codes.values)).batch(
+        BATCH_SIZE
+    )
+
     hist = model.fit(
-        synth_train_dataset,
-        validation_data=synth_val_dataset,
+        train_dataset,
+        validation_data=val_dataset,
         epochs=NUM_EPOCHS,
         callbacks=[early_stopping_cb],
     )
 
-    val_loss, val_accuracy = model.evaluate(synth_val_dataset)
+    val_loss, val_accuracy = model.evaluate(val_dataset)
     print(f"[INFO] Best validation acc = {val_accuracy}")
     # pd.DataFrame(hist.history)[["loss", "val_loss"]].plot(figsize=(8, 5))
 
-    model.save_weights("../artefacts/model_synthdata.hdf5")
+    model.save_weights("../artefacts/model_synthdata_and_realdata.hdf5")
 
     if SAVE_TO_S3:
         bucket.upload_file(
-            "../artefacts/model_synthdata.hdf5", "artefacts/model_synthdata.hdf5"
+            "../artefacts/model_synthdata_and_realdata.hdf5",
+            "artefacts/model_synthdata_and_realdata.hdf5",
         )
     print("[INFO] Model saved to disk & uploaded to S3.")
-else:
-    bucket.download_file(
-        "artefacts/model_synthdata.hdf5", "../artefacts/model_synthdata.hdf5"
-    )
-    model.load_weights("../artefacts/model_synthdata.hdf5")
 
 #%%
-# ----------------------------- Continue on Real Data -----------------------------
-train = pd.read_parquet("../data/train.parquet")
-val = pd.read_parquet("../data/val.parquet")
-test = pd.read_parquet("../data/test.parquet")
+# ----------------------------------------- JUST on real data -----------------------------------------
+if JUST_ON_REAL:
+    train = pd.read_parquet("../data/train.parquet")
+    val = pd.read_parquet("../data/val.parquet")
+    test = pd.read_parquet("../data/test.parquet")
 
-xtrain, ytrain = encode_dataframe(encoder, data=train, mode="pytorch")
-xval, yval = encode_dataframe(encoder, data=val, mode="pytorch")
-xtest, ytest = encode_dataframe(encoder, data=test, mode="pytorch")
+    xtrain, ytrain = encode_dataframe(encoder, data=train, mode="pytorch")
+    xval, yval = encode_dataframe(encoder, data=val, mode="pytorch")
+    xtest, ytest = encode_dataframe(encoder, data=test, mode="pytorch")
 
-# Pad my input sequence with zeros
-xtrain = nn.utils.rnn.pad_sequence(sequences=xtrain, batch_first=True, padding_value=0.0)
-xval = nn.utils.rnn.pad_sequence(sequences=xval, batch_first=True, padding_value=0.0)
-xtest = nn.utils.rnn.pad_sequence(sequences=xtest, batch_first=True, padding_value=0.0)
-
-train_dataset = tf.data.Dataset.from_tensor_slices(
-    (xtrain, ytrain.cat.codes.values)
-).batch(BATCH_SIZE)
-val_dataset = tf.data.Dataset.from_tensor_slices((xval, yval.cat.codes.values)).batch(
-    BATCH_SIZE
-)
-
-
-hist = model.fit(
-    train_dataset,
-    validation_data=val_dataset,
-    epochs=NUM_EPOCHS,
-    callbacks=[early_stopping_cb],
-)
-
-val_loss, val_accuracy = model.evaluate(val_dataset)
-print(f"[INFO] Best validation acc = {val_accuracy}")
-# pd.DataFrame(hist.history)[["loss", "val_loss"]].plot(figsize=(8, 5))
-
-model.save_weights("../artefacts/model_synthdata_and_realdata.hdf5")
-
-if SAVE_TO_S3:
-    bucket.upload_file(
-        "../artefacts/model_synthdata_and_realdata.hdf5", "artefacts/model_synthdata_and_realdata.hdf5"
+    # Pad my input sequence with zeros
+    xtrain = nn.utils.rnn.pad_sequence(
+        sequences=xtrain, batch_first=True, padding_value=0.0
     )
-print("[INFO] Model saved to disk & uploaded to S3.")
+    xval = nn.utils.rnn.pad_sequence(sequences=xval, batch_first=True, padding_value=0.0)
+    xtest = nn.utils.rnn.pad_sequence(
+        sequences=xtest, batch_first=True, padding_value=0.0
+    )
+
+    train_dataset = tf.data.Dataset.from_tensor_slices(
+        (xtrain, ytrain.cat.codes.values)
+    ).batch(BATCH_SIZE)
+    val_dataset = tf.data.Dataset.from_tensor_slices((xval, yval.cat.codes.values)).batch(
+        BATCH_SIZE
+    )
+
+    hist = model.fit(
+        train_dataset,
+        validation_data=val_dataset,
+        epochs=NUM_EPOCHS,
+        callbacks=[early_stopping_cb],
+    )
+
+    val_loss, val_accuracy = model.evaluate(val_dataset)
+    print(f"[INFO] Best validation acc = {val_accuracy}")
+    # pd.DataFrame(hist.history)[["loss", "val_loss"]].plot(figsize=(8, 5))
+
+    model.save_weights("../artefacts/model_just_realdata.hdf5")
+
+    if SAVE_TO_S3:
+        bucket.upload_file(
+            "../artefacts/model_just_realdata.hdf5", "artefacts/model_just_realdata.hdf5"
+        )
+    print("[INFO] Model saved to disk & uploaded to S3.")
